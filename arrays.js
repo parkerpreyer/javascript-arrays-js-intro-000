1c822a914bdf614b7453bc7a1cf885a756aac18f
Parker Preyer <parker.preyer@gmail.com>
2019-10-29 16:35:34 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -1,9 +1,9 @@
-var chocolateBars = ['snickers', 'hundred grand', 'kitkat', 'skittles']
+const chocolateBars = ['snickers', 'hundred grand', 'kitkat', 'skittles']
 
 function addElementToBeginningOfArray(array, element) {
   array.unshift(element)
 }
 
-function destructivelyAddElementToBeginningOfArray(array, element) {
-
+function destructivelyAddElementToBeginningOfArray(arr, element) {
+  const arr2 = [element, ...arr]
 }
