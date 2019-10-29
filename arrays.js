2294e2bfb9cd3c4027f114e93abea9ee06967749
Parker Preyer <parker.preyer@gmail.com>
2019-10-29 16:39:15 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -2,16 +2,22 @@ const chocolateBars = ['snickers', 'hundred grand', 'kitkat', 'skittles']
 
 function addElementToBeginningOfArray(array, element) {
   const arr2 = [element, ...arr]
+  return arr2
 }
 
 function destructivelyAddElementToBeginningOfArray(arr, element) {
-  array.unshift(element)
+  return array.unshift(element)
 }
 
 function addElementToEndOfArray(arr, element) {
   const arr2 = [...arr, element]
+  return arr2
 }
 
 function destructivelyAddElementToEndOfArray(arr, element) {
-  arr.push(element)
+  return arr.push(element)
+}
+
+function accessElementInArray(arr, index) {
+  return arr[index]
 }
